f489fbb5154a8ece3c0c1120e29dd02c4ac08ee1
soundaryamurugesan <30317674+soundaryamurugesan@users.noreply.github.com>
2017-07-20 18:02:09 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main();
+{
+ int sum;
+ printf("enter the number");
+ scanf("%d" ,&num);
+if(num>0)
+{
+ printf("number is positive");
+ }
+ else if(num<0)
+ {
+  printf("number is negative");
+  }
+  else
+  {
+   printf("number is zero");
+   }
+   return 0;
+   }
+
+ 
